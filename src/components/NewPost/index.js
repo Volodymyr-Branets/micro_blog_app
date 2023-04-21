621f70ff7603992ce202995e42a4fcd68c766a86
Volodymyr <branets.volodymyr@gmail.com>
2023-04-21 22:46:22 +0300

    Add functionality to NewPost component, make some changes to PostDetail component

--- src/components/NewPost/index.js
+++ src/components/NewPost/index.js
@@ -1,8 +1,83 @@
+import {
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+
 const NewPost = () => {
+  const [title, setTitle] = useState("");
+  const [postText, setPostText] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const post = { title, postText, author };
+    console.log(post);
+  };
+
   return (
     <div>
-      <h2>New Post</h2>
+      <Typography align="center" variant="h2">
+        Add new post
+      </Typography>
       <hr />
+      <Card
+        sx={{
+          maxWidth: 500,
+          mx: "auto",
+        }}
+      >
+        <CardContent>
+          <form onSubmit={handleSubmit}>
+            <Grid container spacing={1}>
+              <Grid xs={12} item>
+                <TextField
+                  label="Post title"
+                  placeholder="Enter post title"
+                  variant="outlined"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid xs={12} item>
+                <TextField
+                  label="Post text:"
+                  multiline
+                  rows={4}
+                  placeholder="Write post text"
+                  variant="outlined"
+                  value={postText}
+                  onChange={(e) => setPostText(e.target.value)}
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid xs={12} item>
+                <TextField
+                  label="Author name"
+                  placeholder="Enter author name"
+                  variant="outlined"
+                  value={author}
+                  onChange={(e) => setAuthor(e.target.value)}
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid xs={12} item>
+                <Button type="submit" variant="contained">
+                  Add post
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 };
--- src/components/PostDetails/index.js
+++ src/components/PostDetails/index.js
@@ -1,11 +1,26 @@
 import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 
 const PostDetails = () => {
   const { id } = useParams();
+  const {
+    data: post,
+    isLoading,
+    error,
+  } = useFetch("http://localhost:8000/posts/" + id);
+
   return (
     <div>
-      <h2>Post details - {id}</h2>
-      <hr />
+      {isLoading && <div>Loading...</div>}
+      {error && <div> {error} </div>}
+      {post && (
+        <article>
+          <h2>{post.title}</h2>
+          <p>Written by {post.author}</p>
+          <hr />
+          <div>{post.text}</div>
+        </article>
+      )}
     </div>
   );
 };
